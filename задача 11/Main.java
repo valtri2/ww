3e78c8169eb0f3b341c6495d174518ca1f088557
valtri2 <74109479+valtri2@users.noreply.github.com>
2020-11-08 14:19:08 +0300

    Create Main.java

---
+++ задача 11/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+       /*  */
+
+        String name = "ww";
+         String city = "mm";
+        int age = 1;
+        System.out.println(name);
+        System.out.println(city);
+        System.out.println(age);
+    }
+}
