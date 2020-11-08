e8d23ff573cbd40f9e8f9b2215157415f8882d4e
valtri2 <74109479+valtri2@users.noreply.github.com>
2020-11-08 17:37:15 +0300

    Create Main.java

---
+++ задача 16/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+          int TF = scan.nextInt();
+          double a ;
+          a= TF * 1.15;
+        System.out.println(a);
+        }
+    }
