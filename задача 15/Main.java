4f650a9b541e72236dede40c74b06742fb4c1c55
valtri2 <74109479+valtri2@users.noreply.github.com>
2020-11-08 16:42:00 +0300

    Create Main.java

---
+++ задача 15/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+ double TC;
+        TC = 41;
+        TC =9 * TC/ 5;
+
+double TF = TC + 32;
+
+        System.out.print(TF);
+    }
+}
