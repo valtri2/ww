5907a427aed386aef9bc18e8008219887750db10
valtri2 <74109479+valtri2@users.noreply.github.com>
2020-11-09 23:03:56 +0300

    Create Main.java

---
+++ task24/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+// 'это сам'
+        double a = 100;
+        for (int i = 1; i <= 5; i++) {
+a=a*1.05;
+            System.out.println(a);
+                    }
+            }
+        }
