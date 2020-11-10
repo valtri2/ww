384616e56a69c4f5beec00482856abc32fd994b2
valtri2 <74109479+valtri2@users.noreply.github.com>
2020-11-10 09:58:19 +0300

    Create Main.java

---
+++ task25/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+// 'это сам'
+        Scanner myscan = new Scanner (System.in);
+        System.out.println ("Введи первое число");
+        double a = 10;
+        double b = 0;
+        b = myscan.nextDouble();
+        System.out.println(a==b ? "верно" : "не верно");
+                    }
+            }
