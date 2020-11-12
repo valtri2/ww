23949a72b608b320d73fffbbf109dd3f5daa8ee6
valtri2 <74109479+valtri2@users.noreply.github.com>
2020-11-12 21:27:57 +0300

    Create Main.java

---
+++ задача 33/Main.java
@@ -0,0 +1,17 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. 
+Если эти числа равны - пусть функция вернет true,
+ а если не равны - false.
+*/
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+       
+    System.out.println(po(4,4));}
+        public static boolean po(int a, int b){
+            if (a == b)
+                return true;
+            else
+            return  false;
+        }
+    }
