cc9a102e7c09218cc3de330dc6b2788349fe2212
valtri2 <74109479+valtri2@users.noreply.github.com>
2020-11-12 21:36:10 +0300

    Create Main.java

---
+++ задача 34/Main.java
@@ -0,0 +1,17 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа.
+Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
+*/
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    System.out.println(po(7,4));}    
+        public static boolean po(int a, int b){
+            int c =0;
+            c= a+b;
+            if (c>10)
+                return true;
+            else
+            return  false;
+        }
+    }
